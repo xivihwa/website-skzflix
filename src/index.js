23b48d832c078043737492ffa54e07287c5dfc97
xivihwa <122834118+xivihwa@users.noreply.github.com>
2023-08-10 19:58:17 +0300

    main page skzflix

---
+++ src/index.js
@@ -0,0 +1,48 @@
+let accordian = document.getElementsByClassName("FAQ__title");
+
+for (let i = 0; i < accordian.length; i++) {
+  accordian[i].addEventListener("click", function () {
+    if (this.childNodes[1].classList.contains("fa-plus")) {
+      this.childNodes[1].classList.remove("fa-plus");
+      this.childNodes[1].classList.add("fa-times");
+    } else {
+      this.childNodes[1].classList.remove("fa-times");
+      this.childNodes[1].classList.add("fa-plus");
+    }
+
+    let content = this.nextElementSibling;
+    if (content.style.maxHeight) {
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  });
+}
+
+const allLang = ["en", "ua"];
+const select = document.querySelector("select");
+select.addEventListener("change", changeURLlanguage);
+
+function changeURLlanguage() {
+  let language = select.value;
+  location.href = window.location.pathname + "#" + language;
+  location.reload();
+}
+
+function changeLanguage() {
+  let hash = window.location.hash;
+  hash = hash.substr(1);
+  console.log(hash);
+  if (!allLang.includes(hash)) {
+    location.href = window.location.pathname + "#en";
+    location.reload();
+  }
+  select.value = hash;
+  for (let key in langArr) {
+    let element = document.querySelector(".lng-" + key);
+    if (element) {
+      element.innerHTML = langArr[key][hash];
+    }
+  }
+}
+changeLanguage();
---
+++ src/language.js
@@ -0,0 +1,134 @@
+const langArr = {
+  subtitle: {
+    en: "Watch anywhere.",
+    ua: "Дивіться будь-де.",
+  },
+  description: {
+    en: "Ready to watch? Choose the video you're looking for.",
+    ua: "Готові дивитись? Виберіть відео, яке ви шукали.",
+  },
+  musicTitle: {
+    en: "Music",
+    ua: "Музика",
+  },
+  musicDescription: {
+    en: "All albums Stray Kids are collected here - from the very beginning of their career to the present day.",
+    ua: "Тут зібрані всі альбоми Stray Kids - від самого початку їх кар'єри до сьогодення.",
+  },
+  playButton: {
+    en: "Play",
+    ua: "Слухати",
+  },
+  liveDescription: {
+    en: "There are V Live from Channie's room, Maknae's private life, Hyunjin's serias and all the VLives of other members of the group.",
+    ua: "Тут є V Live з кімнати Чана, особисте життя Макне, серії Хьонджина та всі VLive інших учасників гурту.",
+  },
+  watchButton: {
+    en: "Watch",
+    ua: "Дивитись",
+  },
+  originalDescription: {
+    en: "Here you can find all the film making, dance practice, two kids' rooms and much more.",
+    ua: "Тут можна знайти всі зйомки кліпів, репетиції танців, кімнати з двома дітьми та багато іншого.",
+  },
+  shows: {
+    en: "Mnet Shows and TV/Radio",
+    ua: "Mnet шоу та ТВ/Радіо",
+  },
+  showsDescription: {
+    en: "Look at the Mnet show where the Stray kids were performing.",
+    ua: "Дивіться шоу Mnet, де виступали Stray kids.",
+  },
+  questions: {
+    en: "Frequently Asked Questions",
+    ua: "Найбільш поширені запитання",
+  },
+  skzflix: {
+    en: "What is Skzflix?",
+    ua: "Що таке Skzflix?",
+  },
+  answerSkz: {
+    en: "Skzflix is a compilation of all the YouTube videos related to Stray Kids.",
+    ua: "Skzflix - це збірка всіх відео на YouTube, пов'язаних з Stray Kids.",
+  },
+  answerSkzflix: {
+    en: "You can watch as much as you want, whenever you want, without a single ad. There's always something new to discover.",
+    ua: "Ви можете дивитися скільки завгодно, коли завгодно, без без жодної реклами. Завжди можна відкрити для себе щось нове.",
+  },
+  skz: {
+    en: "What is Stray Kids?",
+    ua: "Хто такі Stray Kids?",
+  },
+  answerSkz: {
+    en: "Stray Kids is a South Korean boy band formed by JYP Entertainment through the 2017 reality show of the same name. The group is composed of eight members: Bang Chan, Lee Know, Changbin, Hyunjin, Han, Felix, Seungmin, and I.N. Originally a nine-piece group, member Woojin left due to undisclosed personal reasons in October 2019.",
+    ua: "Stray Kids - південнокорейська чоловіча група, створена компанією JYP Entertainment завдяки однойменному реаліті-шоу 2017 року. Гурт складається з восьми учасників: Бан Чан, Лі Ноу, Чанбін, Хьонджин, Хан, Фелікс, Синмін та І.Н. Спочатку група складалася з дев'яти учасників, учасник Вуджин покинув її через нерозкриті особисті причин у жовтні 2019 року.",
+  },
+  bangchan: {
+    en: "Who is Bang Chan?",
+    ua: "Хто такий Бан Чан?",
+  },
+  answerBangchan: {
+    en: "Christopher Bang Chan was born on October 3, 1997 in Seoul, South Korea. He is the leader, producer, vocalist and dancer of the group Stray Kids.",
+    ua: "Крістофер Бан Чан народився 3 жовтня 1997 року в Сеулі, Південна Корея. Він є лідером, продюсером, вокалістом і танцюристом групи Stray Kids.",
+  },
+  know: {
+    en: "Who is Lee Know?",
+    ua: "Хто такий Лі Ноу?",
+  },
+  answerKnow: {
+    en: "Lee Minho was born on October 25, 1998 in Gimpo, South Korea. He is the main dancer, vocalist and visual of the group Stray Kids.",
+    ua: "Лі Мінхо народився 25 жовтня 1998 року в Гімпо, Південна Корея. Він є головним танцюристом, вокалістом і візуалом групи Stray Kids.",
+  },
+  changbin: {
+    en: "Who is Changbin?",
+    ua: "Хто такий Чанбін?",
+  },
+  answerChangbin: {
+    en: "Seo Changbin was born on August 11, 1999 in Yongin, South Korea. He is the main rapper and producer of the group Stray Kids.",
+    ua: "Со Чанбін народився 11 серпня 1999 року в Йон'ін, Південна Корея. Він є головним репером і продюсером групи Stray Kids.",
+  },
+  hyunjin: {
+    en: "WHo is Hyunjin?",
+    ua: "Хто такий Хьонджин?",
+  },
+  answerHyunjin: {
+    en: "Hwang Hyunjin was born on March 20, 2000 in Seoul, South Korea. He is the main dancer, rapper and visual of the group Stray Kids.",
+    ua: "Хван Хьонджин народився 20 березня 2000 року в Сеулі, Південна Корея. Він є головним танцюристом, репером та візуалом групи Stray Kids.",
+  },
+  han: {
+    en: "Who is Han?",
+    ua: "Хто такий Хан?",
+  },
+  answerHan: {
+    en: "Han Jisung was born on September 14, 2000 in Incheon, South Korea. He is the main rapper, vocalist and producer of the group Stray Kids.",
+    ua: "Хан Джисон народився 14 вересня 2000 року в Інчхоні, Південна Корея. Він є основним репером, вокалістом і продюсером групи Stray Kids.",
+  },
+  felix: {
+    en: "Who is Felix?",
+    ua: "Хто такий Фелікс?",
+  },
+  answerFelix: {
+    en: "Felix Lee (Lee Yongbok) was born on September 15, 2000 in Sydney, Australia. He is the main dancer, rapper and visual of the group Stray Kids.",
+    ua: "Фелікс Лі (Лі Йонбок) народився 15 вересня 2000 року в Сіднеї, Австралія. Він є головним танцюристом, репером і візуалом групи Stray Kids.",
+  },
+  seungmin: {
+    en: "Who is Seungmin?",
+    ua: "Хто такий Синмін?",
+  },
+  answerSeungmin: {
+    en: "Kim Seungmin was born on September 22, 2000 in Seoul, South Korea. He is the main vocalist of the group Stray Kids.",
+    ua: "Кім Синмін народився 22 вересня 2000 року в Сеулі, Південна Корея. Він головний вокаліст групи Stray Kids.",
+  },
+  in: {
+    en: "WHo is I.N.?",
+    ua: "Хто такий І.Н.?",
+  },
+  answerIn: {
+    en: "Yang Jeongin was born on February 8, 2001 in Busan, South Korea. He is the vocalist and maknae of the group Stray Kids.",
+    ua: "Ян Чонін народився 8 лютого 2001 року в Пусані, Південна Корея. Він є вокалістом і макне групи Stray Kids.",
+  },
+  email: {
+    en: "Questions? Call 000 000 000 00 Or email: iiiiiiiiii.gmail.com",
+    ua: "Питання? Дзвоніть на 000 000 000 00 Або пишіть: iiiiiiiiii.gmail.com",
+  },
+};
