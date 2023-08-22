49d3673762c6537df4db426e7fb0e4e3d0c6ae23
xivihwa <antonenko.violetta@lll.kpi.ua>
2023-08-22 12:22:54 +0300

    add logo click to home page

---
+++ src/click.js
@@ -0,0 +1,17 @@
+document.getElementById("playButton").addEventListener("click", function () {
+  window.location.href = "music.html";
+});
+
+document.getElementById("liveButton").addEventListener("click", function () {
+  window.location.href = "live.html";
+});
+
+document
+  .getElementById("originalButton")
+  .addEventListener("click", function () {
+    window.location.href = "original.html";
+  });
+
+document.getElementById("showButton").addEventListener("click", function () {
+  window.location.href = "show.html";
+});
--- src/index.js
+++ src/index.js
@@ -1,21 +1,3 @@
-document.getElementById("playButton").addEventListener("click", function () {
-  window.location.href = "music.html";
-});
-
-document.getElementById("liveButton").addEventListener("click", function () {
-  window.location.href = "live.html";
-});
-
-document
-  .getElementById("originalButton")
-  .addEventListener("click", function () {
-    window.location.href = "original.html";
-  });
-
-document.getElementById("showButton").addEventListener("click", function () {
-  window.location.href = "show.html";
-});
-
 let accordian = document.getElementsByClassName("FAQ__title");
 
 for (let i = 0; i < accordian.length; i++) {
